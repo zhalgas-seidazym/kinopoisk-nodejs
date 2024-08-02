9d5bcd5a14adf649e6983ea5912c5d93cb3d6a00
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-08-03 02:59:23 +0600

    /

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/',
-    {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
-    socketTimeoutMS: 45000 // Increase the socket timeout
-    }
-)
+mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/mydatabase?retryWrites=true&w=majority')
 .then(() => console.log('Connected to MongoDB'))
 .catch((e) => console.log('Failed to connect to MongoDB'));
 
 // zhalgasseidazym2005
 // NXzIpHAJ8Fp7od7U
+// 'mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/'
