19dafe401b7ce281fa7989ffd520d869380f581f
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-08-03 02:37:47 +0600

    /

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/')
+mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/',
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
+    socketTimeoutMS: 45000 // Increase the socket timeout
+    }
+)
 .then(() => console.log('Connected to MongoDB'))
 .catch((e) => console.log('Failed to connect to MongoDB'));
 
