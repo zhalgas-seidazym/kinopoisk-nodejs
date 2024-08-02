6f8e385e80a92e187229bfde1503f31de13c513f
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-08-03 03:24:09 +0600

    /

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/')
 .then(() => console.log('Connected to MongoDB'))
 .catch((e) => console.log('Failed to connect to MongoDB'));
 
