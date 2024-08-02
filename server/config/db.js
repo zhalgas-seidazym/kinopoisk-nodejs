c2e253a4b8bdcf12aa8b0ac0dbd6d0ba376a3398
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-08-03 01:46:00 +0600

    added db mongodb atlass

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/kinopoisk')
+mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/')
 .then(() => console.log('Connected to MongoDB'))
 .catch((e) => console.log('Failed to connect to MongoDB'));
+
+// zhalgasseidazym2005
+// NXzIpHAJ8Fp7od7U
