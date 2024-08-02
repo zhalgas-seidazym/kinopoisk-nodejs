c7f4d1934dbac951ae730f8e8ba2906ce4e4294c
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-08-03 03:12:27 +0600

    /

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/mydatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://zhalgasseidazym2005:NXzIpHAJ8Fp7od7U@kinopoisk-app.95x5lts.mongodb.net/')
 .then(() => console.log('Connected to MongoDB'))
 .catch((e) => console.log('Failed to connect to MongoDB'));
 
